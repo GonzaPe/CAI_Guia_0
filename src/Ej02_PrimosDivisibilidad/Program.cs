d80f9136afbcba301fb767644e53356c9f194886
Gonzalo Hernán Perez <102616078+GonzaPe@users.noreply.github.com>
2026-05-17 16:40:09 -0300

    qwertyu

---
+++ src/Ej02_PrimosDivisibilidad/Program.cs
@@ -0,0 +1,2 @@
+﻿//Ejercicio 2: Primos y Divisibilidad
+//Crear una aplicación que cuente desde el número 1 hasta los últimos tres dígitos de tu número de registro (si empieza con 0, sumar 100). Identificar los múltiplos de 3 y los números primos. Mostrar el número con el mensaje: "Es divisible por 3", "Es primo" o "Es primo y divisible por 3".
