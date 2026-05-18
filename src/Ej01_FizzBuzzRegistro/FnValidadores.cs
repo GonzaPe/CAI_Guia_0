6b621a4ae4ac6f9c0403eeb6fb3b59d6589261ea
Gonzalo Hernán Perez <102616078+GonzaPe@users.noreply.github.com>
2026-05-17 21:00:26 -0300

    Se puede aumentar la cohesión un poco mas pero ya está bien

---
+++ src/Ej01_FizzBuzzRegistro/FnValidadores.cs
@@ -0,0 +1,25 @@
+﻿namespace Ej01_FizzBuzzRegistro;
+public static class FnValidadores
+{
+    public static int PedirValorEnteroRango(string mensaje, int valorMin, int valorMax)
+    {
+        int valorIngresado;
+        bool valida = false;
+
+        do
+        {
+            Console.Write(mensaje);
+            valida = int.TryParse(Console.ReadLine(), out valorIngresado);
+            if (!valida)
+            {
+                Console.WriteLine("Ingrese un valor entero positivo");
+            }
+            else if(valorIngresado< valorMin || valorIngresado > valorMax)
+            {
+                Console.WriteLine($"Ingrese un valor entero entre {valorMin} y {valorMax}");
+                valida = false;
+            }
+        } while (!valida);
+        return valorIngresado;
+    }
+}
--- src/Ej01_FizzBuzzRegistro/Program.cs
+++ src/Ej01_FizzBuzzRegistro/Program.cs
@@ -1,20 +1,38 @@
-﻿namespace Ej01_FizzBuzzRegistro;
+﻿//Ejercicio 1: FizzBuzz de Registro
+//Crear una aplicación que cuente desde el número 1 hasta los últimos tres dígitos de tu propio número de registro (en caso de comenzar con 0, sumarle 100). Todos los números deben mostrarse por pantalla. Para los múltiplos de 3, agregar la palabra “Foo”; para los múltiplos de 5, “Bar”; y para los múltiplos de ambos, “FooBar”.
+//•	Ejemplo: El número 15 mostrará “15 – FooBar”.
+//Pedir ingreso de los 3 dígitos. For iterando while i<=numero. if numero%3 print foo
 
-internal class Program
+namespace Ej01_FizzBuzzRegistro;
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        int numeroRegistro;
 
-        Console.WriteLine("Ejercicio 1 - FizzBuzz de Registro");
-        Console.WriteLine("----------------------------------");
+        numeroRegistro = FnValidadores.PedirValorEnteroRango("Ingrese los tres últimos dígitos de su número de registro: ",0,999);
+        if (numeroRegistro < 100) {
+            numeroRegistro += 100;
+        }
 
-        // TODO:
-        // 1. Pedir o definir los últimos tres dígitos del registro.
-        // 2. Si empieza con 0, sumar 100.
-        // 3. Recorrer desde 1 hasta ese valor.
-        // 4. Mostrar Foo, Bar o FooBar según corresponda.
-
-        Console.WriteLine("Pendiente de implementación.");
+        for (int i = 1; i <= numeroRegistro; i++)
+        {
+            if(i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine($"{i} - FooBar");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine($"{i} - Foo");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine($"{i} - Bar");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
